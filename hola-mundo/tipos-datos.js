16fec2a6e3c2a76c7cf767a5cccfaa21fe5d307c
Juan Sebastian <juan.mun3ra@gmail.com>
2026-02-16 11:26:16 -0500

    tipos de datos, configuraciones

---
+++ hola-mundo/tipos-datos.js
@@ -0,0 +1,42 @@
+// ---------- Datos Primitivos ----------
+
+// String
+let nombre = "Juan";
+let nombre2 = "Sebastián";
+
+// Number
+let edad = 25;
+
+// Boolean
+let esMayorDeEdad = true;
+
+// Null
+let noHayValor = null;
+
+// Undefined
+let valorIndefinido; // Sin Asignación
+
+// Symbol
+let simboloUnico = Symbol("mi-simbolo");
+
+// BigInt
+let numeroGrande = 2n;
+
+// ---------- Datos Complejos ----------
+
+// Objetos
+let carro = {
+  marca: "Toyota",
+  modelo: "Model S",
+  color: "Gris Plateado",
+  año: 2019,
+};
+
+// Arrays
+let frutas = ["manzana", "pera", "pera"];
+console.log(frutas);
+
+// Funciones
+function saludar(n) {
+  console.log("Hola " + n);
+}
